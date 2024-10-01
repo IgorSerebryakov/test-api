f722c4819d57736b7a9a9e3c68506477451b47de
IgorSerebryakov <serebryakov09@mail.ru>
2024-10-01 17:02:28 +0300

    commit 01.10

--- app/Modules/Security/Services/RegisterService.php
+++ app/Modules/Security/Services/RegisterService.php
@@ -4,6 +4,7 @@ namespace App\Modules\Security\Services;
 
 use App\Models\User;
 use App\Modules\Security\DTO\RegisterDTO;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterService
 {
@@ -11,8 +12,9 @@ class RegisterService
     {
         $user = new User();
         $user->name = $registerDTO->name;
-        $user->password = $registerDTO->password;
+        $user->password = Hash::make($registerDTO->password);
         $user->email = $registerDTO->email;
+
         $user->save();
 
         return $user;
